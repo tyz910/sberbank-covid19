848ea6cf52aab611d6ce882040e3559d86737661
Evgeniy Ivanov <tyz910@gmail.com>
2020-04-16 20:35:47 +0300

    ru data loader

--- util/data.py
+++ util/data.py
@@ -1,49 +1,72 @@
+import requests
 import pandas as pd
 
 
-def convert_countries(original_countries_path, confirmed_path, save_path):
-    countries = pd.read_csv(original_countries_path, na_filter=False, index_col='iso_alpha3')
-    countries['name'] = countries['ccse_name']
-    countries = countries.drop(columns=['iso_alpha2', 'iso_numeric', 'official_name', 'ccse_name'])
-    countries.index.name = 'country'
+def update_data():
+    time_series_data = {
+        'confirmed.csv': 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv',
+        'deaths.csv': 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv',
+        'confirmed_ru.csv': 'https://raw.githubusercontent.com/grwlf/COVID-19_plus_Russia/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_RU.csv',
+        'deaths_ru.csv': 'https://raw.githubusercontent.com/grwlf/COVID-19_plus_Russia/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_RU.csv',
+    }
 
-    df = pd.read_csv(confirmed_path, na_filter=False)
-    df = df[['Country/Region', 'Lat', 'Long']]
-    df = df.groupby('Country/Region').mean()
+    for file_name, url in time_series_data.items():
+        r = requests.get(url, allow_redirects=True)
+        if r.status_code == 200:
+            print(file_name, 'loaded')
+            with open('data/time_series/' + file_name, 'wb') as f:
+                f.write(r.content)
 
-    df = pd.merge(countries, df, how='left', left_on=['name'], right_on=['Country/Region']).set_index(countries.index)
-    df.to_csv(save_path)
 
+def read_time_series(countries, filepath):
+    df = pd.read_csv(filepath, na_filter=False)
 
-def clean_time_series(df, countries):
-    df = df.drop(columns=['Lat', 'Long', 'Province/State'])
-    df = df.groupby('Country/Region').sum()
-    df = pd.merge(countries[['name']], df, how='left', left_on=['name'], right_on=['Country/Region']).set_index(countries.index)
+    df = df.rename(columns={
+        'Province_State': 'Country/Region',
+        'Country_Region': 'Province/State',
+    }).drop(columns=[
+        'Lat', 'Long', 'Province/State', 'UID', 'iso2', 'iso3',
+        'code3', 'FIPS', 'Admin2', 'Long_', 'Combined_Key'
+    ], errors='ignore').groupby('Country/Region').sum()
+
+    df = pd.merge(countries[['name']], df, how='inner', left_on=['name'], right_on=['Country/Region'])
+    df.index = countries[countries['name'].isin(df['name'])].index
     df = df.drop(columns=['name'])
 
     return df
 
 
-def read_data(countries_path, confirmed_path, deaths_path):
-    countries = pd.read_csv(countries_path, index_col='country', keep_default_na=False, na_values=[''])
-    confirmed = clean_time_series(pd.read_csv(confirmed_path, na_filter=False), countries)
-    deaths = clean_time_series(pd.read_csv(deaths_path, na_filter=False), countries)
+def combine_time_series(confirmed, deaths):
+    df = pd.DataFrame(
+        [
+            [date, country, num_confirmed, deaths[date][country]]
+            for date in confirmed.columns
+            for num_confirmed, country in zip(confirmed[date], confirmed.index)
+        ],
+        columns=['date', 'country', 'confirmed', 'deaths']
+    )
 
-    data = [[date, country, num_confirmed, deaths[date][country]] for date in confirmed.columns for
-            num_confirmed, country in zip(confirmed[date], confirmed.index)]
-
-    df = pd.DataFrame(data, columns=['date', 'country', 'confirmed', 'deaths'])
     df['date'] = pd.to_datetime(df['date'], infer_datetime_format=True)
-    df = df[df['confirmed'] > 0]
-    df = df.reset_index().drop(columns=['index'])
 
-    return countries, df
+    return df
 
 
-def days_idx(df, days, after=True):
-    date_from = df['date'].max() - pd.to_timedelta(days, unit='d')
+def read_data(update=False):
+    if update:
+        update_data()
 
-    if after:
-        return df[df['date'] > date_from].index
-    else:
-        return df[df['date'] <= date_from].index
+    countries = pd.read_csv('data/countries.csv', index_col='country')
+
+    confirmed_global = read_time_series(countries, 'data/time_series/confirmed.csv')
+    deaths_global = read_time_series(countries, 'data/time_series/deaths.csv')
+    data_global = combine_time_series(confirmed_global, deaths_global)
+
+    confirmed_ru = read_time_series(countries, 'data/time_series/confirmed_ru.csv')
+    deaths_ru = read_time_series(countries, 'data/time_series/deaths_ru.csv')
+    data_ru = combine_time_series(confirmed_ru, deaths_ru)
+
+    data = data_global.append(data_ru)
+    data = data[data['confirmed'] > 0]
+    data = data.sort_values(by=['country', 'date']).reset_index(drop=True)
+
+    return countries, data
--- util/features.py
+++ util/features.py
@@ -1,13 +1,22 @@
 import numpy as np
 
 
-def add_prev_values(df, col, days):
+def add_prev_values(df, days):
+    df = df.copy()
+
     for i in range(1, days + 1):
-        df["{}_prev_{}".format(col, i)] = df.groupby("country")[col].shift(i)
-    df.fillna(0, inplace=True)
+        for col in ['confirmed', 'deaths']:
+            df[f"{col}_prev{i}"] = df.groupby("country")[col].shift(i)
+
+    return df.fillna(0)
 
 
 def add_days_since_start(df):
+    df = df.copy()
+
     df['days_since_start'] = 0
     for c in df['country'].unique():
         df.loc[df['country'] == c, 'days_since_start'] = np.arange(len(df[df['country'] == c]))
+
+    return df
+
